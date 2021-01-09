f77f7be53c60c149a968a300eaa5c2c6303bd61f
nanthuzar <nanthuzaraung2207@gmail.com>
2021-01-09 12:41:22 +0630

    finish quiz function not include design

---
+++ activity.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+ /*Array of Objects*/
+const quiz = [
+	{
+		q:'<img src=images/1.png>',
+		options:['smile', 'cry', 'happy'],
+		answer:1
+
+	},
+	{
+		q:'<img src=images/2.png>',
+		options:['sad', 'smile', 'happy'],
+		answer:0
+	},
+	{
+		q:'<img src=images/3.png>',
+		options:['smile', 'sad', 'normal'],
+		answer:2
+
+	},
+	{
+		q:'<img src=images/4.png>',
+		options:['smile', 'happy', 'normal'],
+		answer:0
+	},
+	{
+		q:'<img src=images/5.png>',
+		options:['smile', 'happy', 'cry'],
+		answer:0
+
+	},
+	{
+		q:'<img src=images/1.png>',
+		options:['smile', 'cry', 'happy'],
+		answer:1
+
+	},
+	{
+		q:'<img src=images/2.png>',
+		options:['sad', 'smile', 'happy'],
+		answer:0
+	},
+	{
+		q:'<img src=images/3.png>',
+		options:['smile', 'sad', 'normal'],
+		answer:2
+
+	},
+	{
+		q:'<img src=images/4.png>',
+		options:['smile', 'happy', 'normal'],
+		answer:0
+	},
+	{
+		q:'<img src=images/5.png>',
+		options:['smile', 'happy', 'cry'],
+		answer:0
+
+	}
+]
