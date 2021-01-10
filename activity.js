e87f5ccd2dcfc72171a3e1444d8b7b6a20c602ae
nanthuzar <nanthuzaraung2207@gmail.com>
2021-01-11 04:54:45 +0630

    modified final version

--- activity.js
+++ activity.js
@@ -7,59 +7,59 @@
  /*Array of Objects*/
 const quiz = [
 	{
-		q:'<img src=images/1.png>',
-		options:['smile', 'cry', 'happy'],
-		answer:1
-
-	},
-	{
-		q:'<img src=images/2.png>',
-		options:['sad', 'smile', 'happy'],
-		answer:0
-	},
-	{
-		q:'<img src=images/3.png>',
-		options:['smile', 'sad', 'normal'],
+		q:'<img src=images/quiz/aeroplane.jpg>',
+		options:['car', 'bike', 'aeroplane'],
 		answer:2
 
 	},
 	{
-		q:'<img src=images/4.png>',
-		options:['smile', 'happy', 'normal'],
-		answer:0
-	},
-	{
-		q:'<img src=images/5.png>',
-		options:['smile', 'happy', 'cry'],
-		answer:0
-
-	},
-	{
-		q:'<img src=images/1.png>',
-		options:['smile', 'cry', 'happy'],
+		q:'<img src=images/quiz/aunt.png>',
+		options:['son', 'aunt', 'uncle'],
 		answer:1
-
 	},
 	{
-		q:'<img src=images/2.png>',
-		options:['sad', 'smile', 'happy'],
-		answer:0
-	},
-	{
-		q:'<img src=images/3.png>',
-		options:['smile', 'sad', 'normal'],
+		q:'<img src=images/quiz/bird.jpg>',
+		options:['bee', 'bag', 'bird'],
 		answer:2
 
 	},
 	{
-		q:'<img src=images/4.png>',
-		options:['smile', 'happy', 'normal'],
+		q:'<img src=images/quiz/bag.jpg>',
+		options:['bag', 'bell', 'boy'],
 		answer:0
 	},
 	{
-		q:'<img src=images/5.png>',
-		options:['smile', 'happy', 'cry'],
+		q:'<img src=images/quiz/car.jpg>',
+		options:['chair', 'cup', 'car'],
+		answer:2
+
+	},
+	{
+		q:'<img src=images/quiz/crab.jpg>',
+		options:['cat', 'crab', 'bird'],
+		answer:1
+
+	},
+	{
+		q:'<img src=images/quiz/dig.png>',
+		options:['dig', 'dance', 'deer'],
 		answer:0
+	},
+	{
+		q:'<img src=images/quiz/door.jpg>',
+		options:['dog', 'door', 'desk'],
+		answer:1
+
+	},
+	{
+		q:'<img src=images/quiz/eel.jpg>',
+		options:['eat', 'eyebrowl', 'eel'],
+		answer:2
+	},
+	{
+		q:'<img src=images/quiz/elephant.jpg>',
+		options:['eye', 'elephant', 'ear'],
+		answer:1
 
 	}
 ]
